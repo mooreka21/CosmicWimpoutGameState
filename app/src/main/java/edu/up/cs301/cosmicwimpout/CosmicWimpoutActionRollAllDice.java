3b4c4e469dde6f6d6b99980fe34e023422759a6f
slemly <slemly@outlook.com>
2019-03-26 11:56:30 -0700

    Added RollAllDice and RollSelectedDie - These classes don't work just yet, I'm not sure how to implement the dice functionality while adhering to the game framework.

--- app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionRollAllDice.java
+++ app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionRollAllDice.java
@@ -4,9 +4,11 @@ import edu.up.cs301.game.actionMsg.GameAction;
 
 
 public class CosmicWimpoutActionRollAllDice extends GameAction {
-
-
-    public CosmicWimpoutActionRollAllDice(GamePlayer player){
+    private static final long serialVersionUID = 18893347L;
+    public CosmicWimpoutActionRollAllDice(GamePlayer player, diceArray ourDice){
+        for(int i = 0; i < ourDice.length; i++){
+            ourDice[i].rollMe();
+        }
         super(player);
     }
 
--- app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionRollSelectedDie.java
+++ app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionRollSelectedDie.java
@@ -4,8 +4,9 @@ import edu.up.cs301.game.GamePlayer;
 import edu.up.cs301.game.actionMsg.GameAction;
 
 public class CosmicWimpoutActionRollSelectedDie extends GameAction {
-
-    public CosmicWimpoutActionRollSelectedDie(GamePlayer player){
+    private static final long serialVersionUID= 95598448L;
+    public CosmicWimpoutActionRollSelectedDie(GamePlayer player, Die ourDie){
+        ourDie.rollMe();
         super(player);
     }
 }
