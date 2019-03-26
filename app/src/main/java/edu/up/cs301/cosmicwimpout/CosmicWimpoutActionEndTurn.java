5e4f74f45d12781be86d546ffef3f139c2930d59
dendinge21 <dendinge21@up.edu>
2019-03-26 09:49:59 -0700

    end turn action class

---
+++ app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionEndTurn.java
@@ -0,0 +1,11 @@
+package edu.up.cs301.cosmicwimpout;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+public class CosmicWimpoutActionEndTurn extends GameAction {
+
+    public CosmicWimpoutActionEndTurn(GamePlayer player){
+        super(player);
+    }
+}
