93f75fce5ca9fa55b690f36da52bd197ce3fec3d
dendinge21 <dendinge21@up.edu>
2019-03-26 09:53:15 -0700

    Other action classes

---
+++ app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionEndGame.java
@@ -0,0 +1,11 @@
+package edu.up.cs301.cosmicwimpout;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+public class CosmicWimpoutActionEndGame extends GameAction {
+
+    public CosmicWimpoutActionEndGame(GamePlayer player){
+        super(player);
+    }
+}
---
+++ app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutActionRollSelectedDie.java
@@ -0,0 +1,11 @@
+package edu.up.cs301.cosmicwimpout;
+
+import edu.up.cs301.game.GamePlayer;
+import edu.up.cs301.game.actionMsg.GameAction;
+
+public class CosmicWimpoutActionRollSelectedDie extends GameAction {
+    
+    public CosmicWimpoutActionRollSelectedDie(GamePlayer player){
+        super(player);
+    }
+}
