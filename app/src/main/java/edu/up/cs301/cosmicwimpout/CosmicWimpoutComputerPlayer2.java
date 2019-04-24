f9881f3f0f583e6eb9a0b587056a93f952562452
slemly <slemly@outlook.com>
2019-04-24 08:26:13 -0700

    Became religious for a minute so I could pray this was my last commit for the Smart AI
    Fixed an issue where the smart AI would end it's turn without updating the score
    Known issue:  Sometimes die faces in the GUI do not properly match with the value on the state - Not sure if this is a state issue or a threading one

--- app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutComputerPlayer2.java
+++ app/src/main/java/edu/up/cs301/cosmicwimpout/CosmicWimpoutComputerPlayer2.java
@@ -6,8 +6,13 @@ import edu.up.cs301.game.infoMsg.GameInfo;
 import android.app.Activity;
 import android.os.Handler;
 import android.util.Log;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import org.w3c.dom.Text;
+
 import java.io.Serializable;
+
 
 
 /**
@@ -24,8 +29,10 @@ import java.io.Serializable;
  * formula, and if the odds of success are over 50%, it rerolls all eligible dice in the active
  * gamestate. If the bot doesn't wimpout or isntantly lose,
  * the number of successes are decremented, to symbolize a successful roll being used. The odds are
- * then recalculated - the turn ends if the odds of success are <50% , if else,
- * the process repeats until the bot scores over 70 points for its turn.
+ * then recalculated - the turn ends if the odds of success are <50%
+ * (Note : Our group agreed <50% odds would be unacceptable to a human if he/she knew them when
+ * that person went to reroll, and based the behaviour of the smart bot on this.)
+ * , if else, the process repeats until the bot scores over 70 points for its turn.
 * 
 * @author Sam Lemly, Olivia Dendinger, David Campbell, Kayla Moore
 *  @version April 2019
@@ -37,9 +44,20 @@ public class CosmicWimpoutComputerPlayer2
     private int numRollsThisTurn;
     private int maxTurnScore = 50;
     private float odds;
-    private int intelligence = 25;
+    private int intelligence = 10;
     private int[] scoresFromCopy = new int[intelligence];
     private int fails;
+    private TextView player1Score;
+    private TextView player2Score;
+    private TextView player3Score;
+    private TextView player4Score;
+    private TextView turnScore;
+    private int redDiceFaces[] = {R.drawable.ten, R.drawable.halfcircles, R.drawable.triangle,
+            R.drawable.bolts, R.drawable.five, R.drawable.stars};
+    private int blackDiceFaces[] = {R.drawable.blackten, R.drawable.blacktwocircles, R.drawable.flamingsun,
+            R.drawable.blackbolt, R.drawable.blackfive, R.drawable.blackstar};
+
+    private ImageView die1, die2, die3, die4, die5;
     //this boolean array helps determine which die are to be kept static in the copy reroll
     private boolean[] needReroll = new boolean[5];
 	/*
@@ -114,7 +132,142 @@ public class CosmicWimpoutComputerPlayer2
 					new Runnable() {
 						public void run() {
 						if (cosmicWimpoutValueTextView != null && state != null) {
-							//cosmicWimpoutValueTextView.setText("" + currentGameState.getCounter());
+                            if (player1Score != null && state != null) {
+                                player1Score.setText(allPlayerNames[0] + ": " + state.getPlayer1Score());
+                                player2Score.setText(allPlayerNames[1] + ": " + state.getPlayer2Score());
+                                player3Score.setText(allPlayerNames[2] + ": " + state.getPlayer3Score());
+                                player4Score.setText(allPlayerNames[3] + ": " + state.getPlayer4Score());
+                                turnScore.setText("Turn Score: " + state.getTurnScore() + "pts");
+
+                                //setting die 1 face to whatever the current die state is
+                                if(state.getDiceVal(0).equals("Tens")){
+                                    die1.setImageResource(redDiceFaces[0]);
+                                }
+                                else if(state.getDiceVal(0).equals("Moons")){
+                                    die1.setImageResource(redDiceFaces[1]);
+                                }
+                                else if(state.getDiceVal(0).equals("Triangles")){
+                                    die1.setImageResource(redDiceFaces[2]);
+                                }
+                                else if(state.getDiceVal(0).equals("Bolts")){
+                                    die1.setImageResource(redDiceFaces[3]);
+                                }
+                                else if(state.getDiceVal(0).equals("Fives")){
+                                    die1.setImageResource(redDiceFaces[4]);
+                                }
+                                else if(state.getDiceVal(0).equals("Stars")){
+                                    die1.setImageResource(redDiceFaces[5]);
+                                }
+
+                                //setting die to face
+                                if(state.getDiceVal(1).equals("Tens")){
+                                    die2.setImageResource(redDiceFaces[0]);
+                                }
+                                else if(state.getDiceVal(1).equals("Moons")){
+                                    die2.setImageResource(redDiceFaces[1]);
+                                }
+                                else if(state.getDiceVal(1).equals("Triangles")){
+                                    die2.setImageResource(redDiceFaces[2]);
+                                }
+                                else if(state.getDiceVal(1).equals("Bolts")){
+                                    die2.setImageResource(redDiceFaces[3]);
+                                }
+                                else if(state.getDiceVal(1).equals("Fives")){
+                                    die2.setImageResource(redDiceFaces[4]);
+                                }
+                                else if(state.getDiceVal(1).equals("Stars")){
+                                    die2.setImageResource(redDiceFaces[5]);
+                                }
+
+                                //setting die 3 face
+                                if(state.getDiceVal(2).equals("Tens")){
+                                    die3.setImageResource(blackDiceFaces[0]);
+                                }
+                                else if(state.getDiceVal(2).equals("Moons")){
+                                    die3.setImageResource(blackDiceFaces[1]);
+                                }
+                                else if(state.getDiceVal(2).equals("Flaming Sun")){
+                                    die3.setImageResource(blackDiceFaces[2]);
+                                }
+                                else if(state.getDiceVal(2).equals("Bolts")){
+                                    die3.setImageResource(blackDiceFaces[3]);
+                                }
+                                else if(state.getDiceVal(2).equals("Fives")){
+                                    die3.setImageResource(blackDiceFaces[4]);
+                                }
+                                else if(state.getDiceVal(2).equals("Stars")){
+                                    die3.setImageResource(blackDiceFaces[5]);
+                                }
+
+                                //setting die 4 face
+                                if(state.getDiceVal(3).equals("Tens")){
+                                    die4.setImageResource(redDiceFaces[0]);
+                                }
+                                else if(state.getDiceVal(3).equals("Moons")){
+                                    die4.setImageResource(redDiceFaces[1]);
+                                }
+                                else if(state.getDiceVal(3).equals("Triangles")){
+                                    die4.setImageResource(redDiceFaces[2]);
+                                }
+                                else if(state.getDiceVal(3).equals("Bolts")){
+                                    die4.setImageResource(redDiceFaces[3]);
+                                }
+                                else if(state.getDiceVal(3).equals("Fives")){
+                                    die4.setImageResource(redDiceFaces[4]);
+                                }
+                                else if(state.getDiceVal(3).equals("Stars")){
+                                    die4.setImageResource(redDiceFaces[5]);
+                                }
+
+                                //setting die 5 face
+                                if(state.getDiceVal(4).equals("Tens")){
+                                    die5.setImageResource(redDiceFaces[0]);
+                                }
+                                else if(state.getDiceVal(4).equals("Moons")){
+                                    die5.setImageResource(redDiceFaces[1]);
+                                }
+                                else if(state.getDiceVal(4).equals("Triangles")){
+                                    die5.setImageResource(redDiceFaces[2]);
+                                }
+                                else if(state.getDiceVal(4).equals("Bolts")){
+                                    die5.setImageResource(redDiceFaces[3]);
+                                }
+                                else if(state.getDiceVal(4).equals("Fives")){
+                                    die5.setImageResource(redDiceFaces[4]);
+                                }
+                                else if(state.getDiceVal(4).equals("Stars")){
+                                    die5.setImageResource(redDiceFaces[5]);
+                                }
+
+                                //highlighting whose turn it is with red background
+                                int turn = state.getWhoseTurn();
+                                if(turn == 0){
+                                    player1Score.setBackgroundColor(0xFFFF0000);
+                                    player2Score.setBackgroundColor(0xFFB7B1A3);
+                                    player3Score.setBackgroundColor(0xFFB7B1A3);
+                                    player4Score.setBackgroundColor(0xFFB7B1A3);
+                                }
+                                if(turn == 1){
+                                    player2Score.setBackgroundColor(0xFFFF0000);
+                                    player1Score.setBackgroundColor(0xFFB7B1A3);
+                                    player3Score.setBackgroundColor(0xFFB7B1A3);
+                                    player4Score.setBackgroundColor(0xFFB7B1A3);
+                                }
+                                if(turn == 2){
+                                    player3Score.setBackgroundColor(0xFFFF0000);
+                                    player1Score.setBackgroundColor(0xFFB7B1A3);
+                                    player2Score.setBackgroundColor(0xFFB7B1A3);
+                                    player4Score.setBackgroundColor(0xFFB7B1A3);
+                                }
+                                if(turn == 3){
+                                    player4Score.setBackgroundColor(0xFFFF0000);
+                                    player1Score.setBackgroundColor(0xFFB7B1A3);
+                                    player2Score.setBackgroundColor(0xFFB7B1A3);
+                                    player3Score.setBackgroundColor(0xFFB7B1A3);
+                                }
+                                //cosmicWimpoutValueTextView.setText("" + currentGameState.getCounter());
+                            }
+
 						}
 					}});
 		}
@@ -148,6 +301,18 @@ public class CosmicWimpoutComputerPlayer2
 		
 		// Load the layout resource for the our GUI's configuration
 		activityForGui.setContentView(R.layout.cosmicwimpout_human_player);
+        this.player1Score = activityForGui.findViewById(R.id.player1Score);
+        this.player2Score = activityForGui.findViewById(R.id.player2Score);
+        this.player3Score = activityForGui.findViewById(R.id.player3Score);
+        this.player4Score = activityForGui.findViewById(R.id.player4Score);
+        this.turnScore = activityForGui.findViewById(R.id.turnscore);
+
+
+        this.die1 = activityForGui.findViewById(R.id.die1);
+        this.die2 = activityForGui.findViewById(R.id.die2);
+        this.die3 = activityForGui.findViewById(R.id.die3);
+        this.die4 = activityForGui.findViewById(R.id.die4);
+        this.die5 = activityForGui.findViewById(R.id.die5);
 
 		
 		// if the state is non=null, update the display
@@ -165,18 +330,64 @@ public class CosmicWimpoutComputerPlayer2
      */
 	private void runSmartAi(GameInfo info){
 	    //int currentTurn = -1;
-	    try {
+
+        this.needReroll = this.state.findScoringDice();
+        getScoresFromCopy(this.state);
+        int successes = getSuccess(this.scoresFromCopy);
+        int rerollCount = scoresFromCopy.length;
+        int turnScoreBefore = this.state.getTurnScore();
+        this.odds = calcOdds(successes, rerollCount);
+        Log.i("***Initial odds: ", this.odds+"**********");
+
+        while (this.odds > 0.5) {
+            //this.state = (CosmicWimpoutState)info;
+            sleep(1000);
+            //flash handling, in case the bot has rolled a flash on the roll prior.
+            //This includes the initial roll of the turn.
+            if(this.state.getIsFlash()){
+                //get the booleans (true if they have to re roll)
+                boolean[] reRolls = this.state.flashReRoll();
+                boolean die1 = reRolls[0];
+                boolean die2 = reRolls[1];
+                boolean die3 = reRolls[2];
+                boolean die4 = reRolls[3];
+                boolean die5 = reRolls[4];
+
+                //send the action with the must re rolls
+                CosmicWimpoutActionRollSelectedDie selectedDie =
+                        new CosmicWimpoutActionRollSelectedDie(this,die1, die2,die3,die4,die5);
+                game.sendAction(selectedDie);
+                //update display
+                updateDisplay();
+            }
+
             this.needReroll = this.state.findScoringDice();
-            getScoresFromCopy(this.state);
-			int successes = getSuccess(this.scoresFromCopy);
-			int rerollCount = scoresFromCopy.length;
-			int turnScoreBefore = this.state.getTurnScore();
-			this.odds = calcOdds(successes, rerollCount);
-            Log.i("***Initial odds: ", this.odds+"**********");
-            while (this.odds > 0.5) {
-                sleep(750);
-                //flash handling, in case the bot has rolled a flash on the roll prior.
-                //This includes the initial roll of the turn.
+            //this rolls the selected dice for the bot
+            CosmicWimpoutActionRollSelectedDie botRollsSomeDice =
+                    new CosmicWimpoutActionRollSelectedDie(this,
+                            this.needReroll[0],
+                            this.needReroll[1],
+                            this.needReroll[2],
+                            this.needReroll[3],
+                            this.needReroll[4]);
+            game.sendAction(botRollsSomeDice);
+            this.updateDisplay();
+            this.needReroll = this.state.findScoringDice();
+            Log.i("Score this turn : ", ""+this.state.getTurnScore());
+            /*
+            if the bot rolled a roll that scored it more points than it
+            started that turn with, it ends its turn.
+            */
+            int newScore = this.state.getTurnScore();
+            if (newScore >= turnScoreBefore) {
+                successes--;
+                Log.i(" Success = ",  successes+"" );
+                this.odds = calcOdds(successes, rerollCount);
+                Log.i("New Odds: ", ""+ this.odds);
+            } else if (state.getTurnScore() == 0) {
+                break;
+            }
+            if (this.state.getTurnScore() >= maxTurnScore) {
                 if(this.state.getIsFlash()){
                     //get the booleans (true if they have to re roll)
                     boolean[] reRolls = this.state.flashReRoll();
@@ -193,45 +404,14 @@ public class CosmicWimpoutComputerPlayer2
                     //update display
                     updateDisplay();
                 }
-
-                this.needReroll = this.state.findScoringDice();
-                //this rolls the selected dice for the bot
-                CosmicWimpoutActionRollSelectedDie botRollsSomeDice =
-                        new CosmicWimpoutActionRollSelectedDie(this,
-                                this.needReroll[0],
-                                this.needReroll[1],
-                                this.needReroll[2],
-                                this.needReroll[3],
-                                this.needReroll[4]);
-                game.sendAction(botRollsSomeDice);
-                this.updateDisplay();
-                this.needReroll = this.state.findScoringDice();
-                /*
-                if the bot rolled a roll that scored it more points than it
-                started that turn with, it ends its turn.
-                */
-                if (this.state.getTurnScore() >= turnScoreBefore) {
-                    successes--;
-                    this.odds = calcOdds(successes, rerollCount);
-                } else {
-                    updateDisplay();
-                    break;
-                }
-                if (this.state.getTurnScore() >= maxTurnScore && !state.getIsFlash()) {
-                    updateDisplay();
-                    break;
-                }
-                //this.needReroll = null;
+                updateDisplay();
+                break;
             }
-
-		}
-		catch(Exception e){
-	    	Log.e("", e + "" );
-		}
+            //this.needReroll = null;
+        }
         CosmicWimpoutActionEndTurn endTurn = new CosmicWimpoutActionEndTurn(this);
         game.sendAction(endTurn);
         updateDisplay();
-
     }
 	/*
 	If the bot scores at all, it'll reroll non-scoring dice only.
